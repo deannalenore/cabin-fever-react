129241a59335e8bd6c50a15cb4a0dde1e984c07e
Cheris Courtney <courtney.cheris@yahoo.com>
2020-06-25 23:06:50 -0400

    added image file & components folder with beachcards.js and beachcard.css. Imported beachcards in index.js and put beachcards in render function

---
+++ src/Components/BeachCards.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './BeachCards.css';
+
+class BeachCards extends Component {
+
+    render() {
+        return(
+            <div>
+                <div className="BeachCards-app">
+                
+                    <div className="BeachCards">beachcard image goes here</div>
+
+                </div>
+            </div>
+        )
+    }
+}
+//js comment to do flip action:document.querySelector("#myCard").classList.toggle("flip")
+export default BeachCards;
